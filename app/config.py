769c58ce7c7cb6ca87edc54fb8516e18a445dce4
Kyle Bowman <Kylebowman99@gmail.com>
2018-08-18 21:14:24 -0400

    Fixed up .gitignore and added config.py

---
+++ app/config.py
@@ -0,0 +1,31 @@
+import logging
+import os
+
+
+class BaseConfig(object):
+    SITE_NAME = 'Compute Cross Product'
+
+    SECRET_KEY = 'houdini'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_BINDS = {
+    #     # These binds allow you to add in multiple databases
+    #     'alias': 'postgresql://postgres:password@localhost:5432/alias'
+    # }
+
+    CLIENT_AUTH_TIMEOUT = 9999
+
+    LOG_LEVEL = logging.INFO
+    LOG_FILENAME = 'activity.log'
+
+
+class DevConfig(BaseConfig):
+    DEBUG = True
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+
+    DEBUG_TB_TEMPLATE_EDITOR_ENABLED = True
+    DEBUG_TB_PROFILER_ENABLED = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    WTF_CSRF_ENABLED = False
